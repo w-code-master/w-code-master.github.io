dc9ecca37aeb3a98b77567a8de4e65bd5aa77b54
MCodeDesigner <mcode.designer@gmail.com>
2019-11-27 15:09:17 +0200

    3456

---
+++ rws/js/_custom.js
@@ -0,0 +1,34 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+$('.main-slider').owlCarousel({
+	items: 1,
+	nav: true,
+	navText: ['<i class="i-left"></i>', '<i class="i-right"></i>'],
+	loop: true
+});
+
+// menu
+$(".sandwich-box").click(function() {
+  $(".sandwich-icon").toggleClass("active");
+});
+$(".nav li a").click(function() {
+	$(".wrapper-nav").fadeOut(600);
+	$(".sandwich-icon").toggleClass("active");
+});
+$(".sandwich-box").click(function() {
+	if($(".wrapper-nav").is(":visible")) {
+		$(".wrapper-nav").fadeOut(600);
+	} else {
+		$(".wrapper-nav").fadeIn(600);
+	};
+});
+
+$('.header__top').animated('fadeInDown');
+$('.main-slider').animated('bounceInUp');
+$('.main-slider__item h1').animated('fadeInDown');
+$('.main-slider__item p').animated('fadeInDown');
+$('.main-slider .owl-dots').animated('fadeInUpBig');
+$('.main-slider .owl-nav .owl-prev').animated('bounceIn');
+$('.main-slider .owl-nav .owl-next').animated('bounceIn');
+
+});
