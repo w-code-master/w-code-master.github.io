37255f00b9e5ef8b3ef4adb77064a438cd27284d
MCodeDesigner <mcode.designer@gmail.com>
2020-04-03 16:07:48 +0300

    23456789

--- azg/js/common.js
+++ azg/js/common.js
@@ -1,5 +1,12 @@
 $(function() {
 
-	// Custom JS
+	// maskedinput
+	$(".masked-input").mask("+7 (999) 999-99-99");
+
+
+	// popup
+	$('.btn-call-popup').magnificPopup({
+		mainClass: 'my-mfp-zoom-in'
+	});
 
 });
