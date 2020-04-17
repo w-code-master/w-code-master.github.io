3a1092a1f27d9acfccf40a17e2f226bb4ed2d9f9
MCodeDesigner <mcode.designer@gmail.com>
2020-04-17 22:03:00 +0300

    2345678

---
+++ o2/js/common.js
@@ -0,0 +1,24 @@
+$(function() {
+
+	// tabs
+	$('.tab__nav--item').click(function() {
+	   var id = $(this).attr('data-tab'),
+	       content = $('.tab__content--item[data-tab="'+ id +'"]');
+	   
+	   $('.tab__nav--item.active').removeClass('active');
+	   $(this).addClass('active');
+	   
+	   $('.tab__content--item.active').removeClass('active');
+	   content.addClass('active');
+	});
+
+	// maskedinput
+	$(".masked-input").mask("+7 (999) 999-99-99");
+
+
+	// popup
+	$('.btn-call-popup').magnificPopup({
+		mainClass: 'my-mfp-zoom-in'
+	});
+
+});
