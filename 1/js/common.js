250fa2957433793d326ba25063cd319c23009375
MCodeDesigner <mcode.designer@gmail.com>
2020-05-28 11:48:31 +0300

    23456

---
+++ 1/js/common.js
@@ -0,0 +1,13 @@
+$(function() {
+
+	// maskedinput
+	$(".masked-input").mask("+7 (999) 999-99-99");
+
+
+	// popup
+	$('.btn-call-popup').magnificPopup({
+		mainClass: 'my-mfp-zoom-in'
+	});
+
+
+});
