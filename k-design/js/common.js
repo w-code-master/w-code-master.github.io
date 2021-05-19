b0fc9dd304590bed09191e2ae0c4631224e0c980
W.CodeMaster <w.code.master@gmail.com>
2021-05-19 22:34:00 +0300

    234567890

--- k-design/js/common.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-
-	$('.beefup').beefup({
-		openSingle: true
-	 });
-
-});
--- super-pol/js/common.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-
-	var headerScene = document.querySelector('.header__scene');
-	var headerSceneInit = new Parallax(headerScene, {
-		frictionY: 0
-	});
-
-});
