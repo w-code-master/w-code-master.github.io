1ef40181c16d7fe936b5b2c15e056fe6f37368bb
W.CodeMaster <w.code.master@gmail.com>
2021-10-13 10:29:33 +0300

    23456789

---
+++ azdigital/thanks.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <title>Azbuka Digital</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <link href="img/favicon.png" rel="shortcut icon" type="image/x-icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta name="format-detection" content="telephone=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tippy.js/2.5.4/tippy.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tippy.js/2.5.4/themes/light.css">
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css?version=123456789101">
+    <meta content="Агентство разработки продающих сайтов и упаковки бизнеса  в СНГ." name="description">
+    <meta content="AzDigital" property="og:title">
+    <meta content="Агентство разработки продающих сайтов и упаковки бизнеса  в СНГ." property="og:description">
+    <meta content="AzDigital" property="twitter:title">
+    <meta content="Агентство разработки продающих сайтов и упаковки бизнеса  в СНГ." property="twitter:description">
+    <meta property="og:type" content="website">
+    <meta content="summary_large_image" name="twitter:card">
+</head>
+
+<body>
+    <div class="content" style="background-image: url('img/bg.jpg')">
+        <header id="header" data-aos="fade-down">
+            <div class="container">
+                <div class="header__left">
+                    <a class="header__logo" href="index.html">
+                        <img src="img/logo.png" alt="">
+                    </a>
+                    <div class="header__description">Online маркетинговое агентство.
+                        <br>Делаем сайты, настраиваем трафик,
+                        <br>приводим клиентов</div>
+                </div>
+                <div class="header__center">
+                    <div class="header__btn modal__open" data-modal="callback">
+                        <img src="img/icons/header/button.png" alt=""><span>Рассчитать предложение</span>
+                    </div>
+                </div>
+                <div class="header__right">
+                    <div class="header__messenger">
+                        <div class="title"> <span>Пишите нам, мы on-line</span>
+                            <div class="green"></div>
+                        </div>
+                        <div class="box">
+                            <a class="link" href="https://t.me/azbukadigital" target="_blank">
+                                <img src="img/icons/header/telegram.png" alt="">
+                            </a>
+                            <a class="link" href="https://wa.me/79524466789?text=%D0%A5%D0%BE%D1%87%D1%83%20%D1%83%D0%B7%D0%BD%D0%B0%D1%82%D1%8C%20%D0%BF%D0%BE%D0%B4%D1%80%D0%BE%D0%B1%D0%BD%D0%B5%D0%B5%20%D0%BE%20%D0%BF%D1%80%D0%B8%D0%B2%D0%BB%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D0%B8%20%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D0%BE%D0%B2"
+                            target="_blank">
+                                <img src="img/icons/header/whatsapp.png" alt="">
+                            </a>
+                            <a class="link" href="https://vk.me/azbukadigital" target="_blank">
+                                <img src="img/icons/header/vk.png" alt="">
+                            </a>
+                        </div>
+                    </div>
+                    <div class="header__phone">
+                        <a class="link" href="tel: 8 952 44 66 789">
+                            <img src="img/icons/header-phone.svg" alt=""><span>8 (952) 44-66-789 </span>
+                        </a>
+                        <div class="description gray">Режим работы: 10.00 - 18.00 по Мск</div>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <img class="round-top" src="img/intro/round-top.png" alt="">
+        <section class="intro-section thanks-section">
+            <img class="main-img" src="img/intro/main.png" alt="">
+            <div class="container">
+                <h1 class="t-title"><b>Отлично!</b> <br class="important">Мы уже получили Вашу заявку и <br>отправили на <b><?php echo $_GET["email"]; ?> и <br><?php echo $_GET["messenger"]; ?></b> 2 полезных подарка <img src="img/icons/ruble.png" alt=""></h1>
+                <div class="thanks-text">Я лично или наш менеджер позвоним вам в ближайшее время, чтобы уточнить пару моментов.</div>
+                <div class="thanks-text">Так мы сможем подготовить для Вас индивидуальные предложения с тремя вариантами
+                    <br>по цене и срокам</div>
+                <div class="thanks-text yellow">Если вы хотите скорректировать время нашего звонка
+                    <br>или у вас есть вопросы,</div>
+                <div class="thanks-text">добавьтесь пожалуйста к руководителю в WhatsApp
+                    <br>по кнопке справа и уточните удобное время
+                    <br>или задайте ваш вопрос</div>
+                <div class="thanks__button">
+                    <img class="vector" src="img/thanks/vector.png" alt="">
+                    <a class="whatsapp" href="https://wa.me/79524466789?text=%D0%97%D0%B4%D1%80%D0%B0%D0%B2%D1%81%D1%82%D0%B2%D1%83%D0%B9%D1%82%D0%B5,%20%D1%85%D0%BE%D1%87%D1%83%20%D1%83%D1%82%D0%BE%D1%87%D0%BD%D0%B8%D1%82%D1%8C%20%D0%BF%D0%B0%D1%80%D1%83%20%D0%B4%D0%B5%D1%82%D0%B0%D0%BB%D0%B5%D0%B9%20%D0%BF%D0%BE%20%D0%BF%D0%BE%D0%B2%D0%BE%D0%B4%D1%83%20%D0%BC%D0%BE%D0%B5%D0%B3%D0%BE%20%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0"
+                    target="_blank">
+                        <img src="img/thanks/whatsapp.svg" alt="">
+                        <div class="anim-circle"></div>
+                        <div class="anim-circle"></div>
+                        <div class="anim-circle"></div>
+                    </a>
+                </div>
+            </div>
+        </section>
+    </div>
+    <div class="overlay"></div>
+    <div class="modal callback" data-modal="callback" style="background-image: url('img/modal/bg.png')">
+        <div class="modal__close">
+            <img src="img/icons/close.png" alt="Close">
+        </div>
+        <div class="title">Получите коммерческое
+            <br>предложение</div>
+        <div class="subtitle">по маркетинговому исследованию
+            <br>для Вашего бизнеса</div>
+        <div class="messenger-title">В каком мессенджере удобно общаться?</div>
+        <form class="callback-form" method="post">
+            <input class="bot" type="text" name="БОТ" value="">
+            <div class="form__inner">
+                <div class="messenger-box">
+                    <label class="label">
+                        <input type="radio" name="Способ_связи" value="Whatsapp" required="">
+                        <div class="item">
+                            <img src="img/icons/whatsapp.png" alt=""><span>Whatsapp</span>
+                        </div>
+                    </label>
+                    <label class="label">
+                        <input type="radio" name="Способ_связи" value="Viber" required="">
+                        <div class="item">
+                            <img src="img/icons/viber.png" alt=""><span>Viber</span>
+                        </div>
+                    </label>
+                    <label class="label">
+                        <input type="radio" name="Способ_связи" value="" required="">
+                        <div class="item">
+                            <img src="img/icons/telegram.png" alt=""><span>Telegram</span>
+                        </div>
+                    </label>
+                </div>
+                <input type="tel" name="Телефон" placeholder="Введите ваш телефон" required="">
+            </div>
+            <button type="submit" class="btn-box">
+                <img class="btn-icon" src="img/intro/rubber-stamp-PVLZ9TB.png" alt="">
+                <div class="intro__main-btn t-btn flowing-scroll">Получить развернутое КП в 3-х
+                    <br>ценовых вариантах <span>(это бесплатно)</span>
+                </div>
+                <div class="btn-shadow"></div>
+            </button>
+        </form>
+        <div class="privacy">
+            <img src="img/icons/privacy.png" alt=""><span>Нажимая кнопку, вы даете согласие на обработку персональных данных и соглашаетесь с <a href="privacy.html" target="_blank"> политикой конфиденциальности</a></span>
+        </div>
+        <img class="bottom-img" src="img/modal/bottom.png" alt="">
+        <img class="pencil-img" src="img/modal/pencil.png" alt="">
+    </div>
+    <footer id="footer">
+        <img class="footer-bg" src="img/footer-bg.png" alt="">
+        <div class="container">
+            <div class="header__left">
+                <a class="header__logo" href="index.html">
+                    <img src="img/footer-logo.png" alt="">
+                </a>
+                <div class="header__description">Online маркетинговое агентство.
+                    <br>Делаем сайты, настраиваем трафик,
+                    <br>приводим клиентов</div>
+            </div>
+            <div class="header__messenger">
+                <div class="title"> <span>Пишите нам, мы on-line</span>
+                    <div class="green"></div>
+                </div>
+                <div class="box">
+                    <a class="link" href="https://t.me/azbukadigital" target="_blank">
+                        <img src="img/icons/header/telegram.png" alt="">
+                    </a>
+                    <a class="link" href="https://wa.me/79524466789?text=%D0%A5%D0%BE%D1%87%D1%83%20%D1%83%D0%B7%D0%BD%D0%B0%D1%82%D1%8C%20%D0%BF%D0%BE%D0%B4%D1%80%D0%BE%D0%B1%D0%BD%D0%B5%D0%B5%20%D0%BE%20%D0%BF%D1%80%D0%B8%D0%B2%D0%BB%D0%B5%D1%87%D0%B5%D0%BD%D0%B8%D0%B8%20%D0%BA%D0%BB%D0%B8%D0%B5%D0%BD%D1%82%D0%BE%D0%B2"
+                    target="_blank">
+                        <img src="img/icons/header/whatsapp.png" alt="">
+                    </a>
+                    <a class="link" href="https://vk.me/azbukadigital" target="_blank">
+                        <img src="img/icons/header/vk.png" alt="">
+                    </a>
+                </div>
+            </div>
+            <div class="header__email header__phone">
+                <a class="link" href="mailto:info@azdigital.com">
+                    <img src="img/icons/footer-email.png" alt=""><span>info@azdigital.com</span>
+                </a>
+                <div class="description">Напишите нам на почту</div>
+            </div>
+            <div class="header__phone">
+                <a class="link" href="tel: 8 952 44 66 789">
+                    <img class="img-svg" src="img/icons/header-phone.svg" alt=""><span>8 (952) 44-66-789 </span>
+                </a>
+                <div class="description">Режим работы: 10.00 - 18.00 по Мск</div>
+            </div>
+            <div class="header__btn modal__open" data-modal="callback">
+                <img src="img/icons/header/button.png" alt=""><span>Рассчитать предложение</span>
+            </div>
+        </div>
+    </footer>
+    <div class="footer__bottom">
+        <div class="container">
+            <div class="privacy title">Я соглашаюсь с <a href="privacy.html" target="_blank">политикой конфиденциальности</a>
+            </div>
+            <div class="title ooo">ООО «Азбука Диджитал», 2012-2021.</div>
+            <div class="title all">Все права защищены</div>
+            <div class="title inn">ИНН <span>7710492060</span> ОГРН <span>1157746187059</span>
+            </div>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/jquery.maskedinput@1.4.1/src/jquery.maskedinput.js"></script>
+    <script src="https://unpkg.com/aos@3.0.0-beta.6/dist/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tippy.js/2.5.4/tippy.all.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <script src="js/main.js?version=1234567891012"></script>
+
+    <!-- Yandex.Metrika counter --> <script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(82308511, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, trackHash:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/82308511" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+</body>
+
+</html>
