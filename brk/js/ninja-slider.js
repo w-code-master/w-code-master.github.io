acefd322637f2418b44697858927b5163fa96ead
W.CodeMaster <w.code.master@gmail.com>
2022-01-26 14:49:39 +0200

    234234324

--- brk/js/ninja-slider.js
+++ brk/js/ninja-slider.js
@@ -202,11 +202,7 @@ function NinjaSlider(a) {
             try {
                 typeof atob == "function" &&
                     (function (a, c) {
-                        var b = yb(
-                            atob(
-                                "dy13QWgsLT9taixPLHowNC1BQStwKyoqTyx6MHoycGlya3hsMTUtQUEreCstd0E0P21qLHctd19uYTJtcndpdnhGaWpzdmksbV9rKCU2NiU3NSU2RSUlNjYlNzUlNkUlNjMlNzQlNjklNkYlNkUlMjAlNjUlMjglKSo8Zy9kYm1tKXVpanQtMio8aCkxKjxoKTIqPGpnKW4+SylvLXAqKnx3YnMhcz5OYnVpL3Nib2VwbikqLXQ+ZAFeLXY+bCkoV3BtaGl2JHR5dmdsZXdpJHZpcW1yaGl2KCotdz4ocWJzZm91T3BlZig8ZHBvdHBtZi9tcGgpcyo8amcpdC9vcGVmT2JuZj4+KEIoKnQ+ayl0KgE8amcpcz8vOSp0L3RmdUJ1dXNqY3Z1ZikoYm11KC12KjxmbXRmIWpnKXM/LzgqfHdic3I+ZXBkdm5mb3UvZHNmYnVmVWZ5dU9wZWYpdiotRz5td3I1PGpnKXM/Lzg2Kkc+R3cvam90ZnN1Q2ZncHNmKXItRypzZnV2c28hdWlqdDw2OSU2RiU2RSU8amcpcz8vOSp0L3RmdUJ1dXNqY3Z1ZikoYm11cGR2bmYlJG91L2RzZmJ1ZlVmeQ=="
-                            ),
-                            a[e] + parseInt(a.charAt(1))
+                        var b = yb(a[e] + parseInt(a.charAt(1))
                         ).substr(0, 3);
                         typeof this[b] === "function" && this[b](c, zb, Ab);
                     })(b, a);
