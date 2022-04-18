1ad4a08698e654a05526a89057aaf1b2dcbf9c55
W.CodeMaster <w.code.master@gmail.com>
2022-04-18 23:10:54 +0200

    234235435

--- medars/js/main.js
+++ medars/js/main.js
@@ -1,6 +1,36 @@
 $(document).ready(function(){
 
+
+	$('.anim-zoomIn').animated('zoomIn');
+	$('.anim-bounceIn').animated('bounceIn');
+	$('.anim-fadeIn').animated('fadeIn');
+	$('.anim-fadeInDown').animated('fadeInDown');
+	$('.anim-fadeInUp').animated('fadeInUp');
+	$('.anim-fadeInUpBig').animated('fadeInUpBig');
+	$('.anim-fadeInRightBig').animated('fadeInRightBig');
+	$('.anim-fadeInLeftBig').animated('fadeInLeftBig');
+	$('.anim-fadeInDownBig').animated('fadeInDownBig');
+	$('.anim-fadeInUpBig').animated('fadeInUpBig');
+
+
+	new Swiper('.swiper-hello', {
+	    effect: 'fade',
+	    fadeEffect: {
+	        crossFade: true
+	    },
+	    pagination: {
+	        el: '.swiper-pagination',
+	        type: 'bullets',
+	        clickable: true
+	    },
+	    navigation: {
+	        nextEl: '.swiper-next-page'
+	    },
+	});
+
+
 	// maskedinput
 	$(".masked-phone").mask("+7 (999) 999-99-99");
+
 
 });
