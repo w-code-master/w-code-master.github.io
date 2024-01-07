8c4b21aee401776a28091ce6c8185b1c298a483b
W.CodeMaster <w.code.master@gmail.com>
2024-01-08 00:52:42 +0200

    1234567890

--- inin/js/main.js
+++ inin/js/main.js
@@ -1,12 +1,24 @@
-$('#person-area-tabs').easytabs();
+$('#person-area-tabs').easytabs({
+   updateHash: false
+});
 
-const swiper = new Swiper('.swiper-company', {
+ $('input[type="tel"]').mask('+7 (999) 999-99-99');
+
+const swiperCompany = new Swiper('.swiper-company', {
    loop: true,
    slidesPerView: "auto",
    autoplay: {
       delay: 2000,
    },
    spaceBetween: 15
- });
+});
 
- $('input[type="tel"]').mask('+7 (999) 999-99-99');
+const swiperReview = new Swiper('.review-swiper', {
+   slidesPerView: 2,
+   loop: true,
+   spaceBetween: 20,
+   navigation: {
+      nextEl: '.review-swiper-next',
+      prevEl: '.review-swiper-prev',
+   }
+});
