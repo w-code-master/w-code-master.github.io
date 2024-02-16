786d3d84739136dbe35c8e0fe1f8cf691e4ba52c
W.CodeMaster <w.code.master@gmail.com>
2024-02-16 22:02:34 +0200

    123213123

---
+++ sintez/js/main.js
@@ -0,0 +1,9 @@
+const swiperReview = new Swiper('.swiper-work', {
+   slidesPerView: 2,
+   loop: true,
+   autoHeight: false,
+   navigation: {
+      nextEl: '.swiper-work-next'
+   },
+   spaceBetween: 20
+});
