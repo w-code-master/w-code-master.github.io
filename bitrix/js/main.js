7a1e029bcf298c773df5bb2dbd093dd628e6ddf8
W.CodeMaster <w.code.master@gmail.com>
2024-08-06 23:16:55 +0300

    345678

--- bitrix/js/main.js
+++ bitrix/js/main.js
@@ -20,7 +20,9 @@ $(function() {
 	$('.anim-slideInUp').animated('slideInUp');
 
 	$('.header-offer-action .btn').magnificPopup({
-		type: 'inline'
+		type: 'inline',
+		removalDelay: 300,
+		mainClass: 'mfp-fade'
 	});
 
 	$('input[type="tel"]').mask('+7 (999) 999-99-99');
