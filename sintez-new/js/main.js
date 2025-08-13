ca6b3b28d1e16767b7e4dea3e7c455b78221c97d
W.CodeMaster <w.code.master@gmail.com>
2025-08-13 22:16:32 +0300

    345678

--- sintez-new/js/main.js
+++ sintez-new/js/main.js
@@ -13,10 +13,23 @@ $(window).scroll(function() {
 
 const reviewsSwiper = new Swiper('.reviews-swiper', {
     loop: true,
-    spaceBetween: 50,
+    spaceBetween: 20,
     slidesPerView: 2,
+    autoHeight: false,
     navigation: {
         nextEl: '.reviews-swiper-next',
         prevEl: '.reviews-swiper-prev',
      },
+     breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        992: {
+            spaceBetween: 50
+            
+        }
+    }
  });
