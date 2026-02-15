98a8487da04624592f958b2563e1e5bcf8366b72
W.CodeMaster <57326546+w-code-master@users.noreply.github.com>
2026-02-15 15:36:28 +0200

    32456789

--- derevodel/js/main.js
+++ derevodel/js/main.js
@@ -1,5 +1,46 @@
+$(document).ready(function() {
+
+// mobile nav
+$(".header__burger").click(function() {
+
+    $(".header__burger").toggleClass("active");
+    $(".mobile-nav").toggleClass("active");
+    
+    $("html").toggleClass("overflow-y-hidden");
+
+    if($(".mobile-nav").is(":visible")) {
+        $(".mobile-nav").fadeOut(0);
+    } else {
+        $(".mobile-nav").fadeIn(0);
+    };
+});
+
+
 const advantagesSlider = new Swiper('.advantages-slider', {
     slidesPerView: "auto",
     centeredSlides: false,
     loop: true
 }); 
+const workingSlider = new Swiper('.working-slider', {
+    slidesPerView: 3,
+    spaceBetween: 15,
+    loop: true,
+    breakpoints: {
+        768: {
+            spaceBetween: 25
+        },
+        1630: {
+            slidesPerView: 4
+        },
+        1760: {
+            spaceBetween: 45
+        }
+    }
+});
+
+$('#catalog-tabs').easytabs({
+    updateHash: false
+});
+
+
+});
