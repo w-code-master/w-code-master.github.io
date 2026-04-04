d318e5478ac1f4937cb06d1066f2af4b593a85c9
W.CodeMaster <57326546+w-code-master@users.noreply.github.com>
2026-04-04 22:39:56 +0300

    q34w567

---
+++ fisherman1/js/main.js
@@ -0,0 +1,144 @@
+$(document).ready(function () {
+	// intlTelInput validation
+	var inputKeyTel = document.querySelector(".inputKeyTel");
+	inputKeyTel.addEventListener("keypress", function (e) {
+		if (e.which < 48 || e.which > 57) {
+			e.preventDefault();
+		}
+	});
+
+	new Swiper(".current-tours-slider", {
+		slidesPerView: 1.35,
+		spaceBetween: 8,
+		loop: true,
+		autoHeight: true,
+		navigation: {
+			nextEl: ".current-tours__next",
+			prevEl: ".current-tours__prev",
+		},
+		breakpoints: {
+			440: {
+				slidesPerView: 1.7,
+			},
+			576: {
+				slidesPerView: 2,
+			},
+			992: {
+				slidesPerView: 3,
+			},
+			1200: {
+				slidesPerView: 4,
+			},
+		},
+
+		pagination: {
+			el: ".swiper-pagination",
+			dynamicBullets: true,
+		},
+	});
+
+	new Swiper(".slider-photo", {
+		slidesPerView: 2,
+		spaceBetween: 6,
+		loop: true,
+		navigation: {
+			nextEl: ".slider-photo__next",
+			prevEl: ".slider-photo__prev",
+		},
+		pagination: {
+			el: ".swiper-pagination",
+			dynamicBullets: true,
+		},
+		breakpoints: {
+			768: {
+				slidesPerView: 3,
+				spaceBetween: 8,
+			},
+			1200: {
+				slidesPerView: 4,
+			},
+		},
+	});
+
+	new Swiper(".slider-image", {
+		slidesPerView: 1,
+		loop: true,
+		navigation: {
+			nextEl: ".slider-image__next",
+			prevEl: ".slider-image__prev",
+		},
+	});
+
+	new Swiper(".slider-photos", {
+		slidesPerView: "auto",
+		speed: 10000,
+		spaceBetween: 8,
+		autoplay: {
+			delay: 0,
+			enabled: true,
+		},
+		freeMode: true,
+		loop: true,
+	});
+
+	new Swiper(".video-slider", {
+		slidesPerView: 1.5,
+		spaceBetween: 8,
+		loop: true,
+		breakpoints: {
+			440: {
+				slidesPerView: 2,
+			},
+			576: {
+				slidesPerView: 3,
+			},
+			992: {
+				slidesPerView: 4,
+			},
+			1200: {
+				slidesPerView: 5,
+			},
+		},
+		pagination: {
+			el: ".swiper-pagination",
+			dynamicBullets: true,
+		},
+	});
+
+	new Swiper(".galleryArticle", {
+		slidesPerView: 3,
+		spaceBetween: 8,
+		loop: true,
+	});
+
+	new Swiper(".sliderCards", {
+		slidesPerView: 1.35,
+		spaceBetween: 6,
+		loop: true,
+		pagination: {
+			el: ".swiper-pagination",
+			dynamicBullets: true,
+		},
+		breakpoints: {
+			440: {
+				slidesPerView: 1.7,
+			},
+		},
+	});
+
+	$(".faq-item").beefup({
+		openSingle: true,
+		trigger: ".faq-head",
+		content: ".faq-body",
+	});
+	$(".spoiler-item").beefup({
+		trigger: ".spoiler-head",
+		content: ".spoiler-body",
+	});
+
+	$(".program-tabs").easytabs({
+		tabs: ".program-tabs__item",
+		defaultTab: ".program-tabs__item:nth-child(1)",
+		updateHash: false,
+	});
+});
