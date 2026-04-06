66dae2fe19f3d689be43e2f445e9cd77a5138677
W.CodeMaster <57326546+w-code-master@users.noreply.github.com>
2026-04-06 23:53:46 +0300

    3456789

---
+++ admirra-app/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	$(".navigation-item.subMenu").beefup({
+		trigger: ".navigation-link",
+		content: ".navigation-submenu",
+	});
+	$(".dropdown").beefup({
+		trigger: ".dropdown-head",
+		content: ".dropdown-body",
+		selfClose: true,
+	});
+	$(".tariff-tabs").easytabs({
+		tabs: ".tariff-tabs__item",
+		defaultTab: ".tariff-tabs__item:nth-child(1)",
+		updateHash: false,
+	});
+});
